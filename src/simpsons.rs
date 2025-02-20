e9caa60025fa8716ce00f24abbbbb2d0ace0ba7b
Kathryn Lovell <krlovell017@gmail.com>
2025-02-19 20:37:51 -0500

    second interval

--- src/simpsons.rs
+++ src/simpsons.rs
@@ -2,8 +2,10 @@ use crate::standard_normal::{round, standard_normal_variable};
 
 pub fn simpsons_rule_approximation(a: f64, b: f64, n: u64, eval: fn(x: f64) -> f64) -> f64 {
     let h = h(a, b, n);
-    let s = initial_s(a, b, eval);
-    return eval(a);
+    return h
+        * (initial_s(a, b, eval)
+            + simpsons_first_loop(a, h, n, eval)
+            + simpsons_second_loop(a, h, n, eval));
 }
 
 fn h(a: f64, b: f64, n: u64) -> f64 {
@@ -20,6 +22,14 @@ fn simpsons_first_loop(a: f64, h: f64, n: u64, eval: fn(x: f64) -> f64) -> f64 {
 
 fn interval(a: f64, h: f64, i: u64, eval: fn(x: f64) -> f64) -> f64 {
     return eval(a + ((i as f64) * h)) / 3.0;
+}
+
+fn simpsons_second_loop(a: f64, h: f64, n: u64, eval: fn(x: f64) -> f64) -> f64 {
+    return (1..=n).fold(0.0, |acc, t| acc + second_interval(a, h, t, eval));
+}
+
+fn second_interval(a: f64, h: f64, i: u64, eval: fn(x: f64) -> f64) -> f64 {
+    return 2.0 * eval(a + (((i as f64) / 2.0) * h)) / 3.0;
 }
 
 #[cfg(test)]
@@ -41,10 +51,16 @@ mod simpsons_rule_tests {
     }
     #[test]
     fn interval_test() {
-        assert_eq!(1 as f64, 1.0);
         assert_eq!(
             round(interval(0.0, 0.1, 1, standard_normal_variable), 12.0),
             0.331670826398
+        );
+    }
+    #[test]
+    fn second_interval_test() {
+        assert_eq!(
+            round(second_interval(0.0, 0.1, 1, standard_normal_variable), 12.0),
+            0.66583385395
         );
     }
     #[test]

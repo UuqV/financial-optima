9e9bd6f384bb87cc3f092aa7acf903b3ae531ec2
Kathryn Lovell <krlovell017@gmail.com>
2025-02-19 19:26:57 -0500

    standard normal variable

--- src/standard_normal.rs
+++ src/standard_normal.rs
@@ -5,8 +5,12 @@ pub fn standard_constants(accumulation: f64) -> f64 {
     return 0.5 + (1.0 / (2.0 * PI)).sqrt() * accumulation;
 }
 
-pub fn round_6_dec(number: f64) -> f64 {
-    return (number * 1000000.0).round() / 1000000.0;
+pub fn round_6_dec(x: f64) -> f64 {
+    return (x * 1000000.0).round() / 1000000.0;
+}
+
+pub fn standard_normal_variable(x: f64) -> f64 {
+    return E.powf(-1.0 * (x.powf(2.0) / 2.0));
 }
 
 #[cfg(test)]
@@ -20,5 +24,9 @@ mod standard_normal_tests {
     #[test]
     fn standard_constants_test() {
         assert_eq!(round_6_dec(standard_constants(E)), 1.584438);
+    }
+    #[test]
+    fn standard_normal_variable_test() {
+        assert_eq!(round_6_dec(standard_normal_variable(2.0)), 0.135335);
     }
 }

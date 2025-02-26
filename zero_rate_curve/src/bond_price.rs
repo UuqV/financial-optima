b8b8b1e0f9f2471239577e487a6ad8f3af5ddb2e
Kathryn Lovell <krlovell017@gmail.com>
2025-02-25 20:31:56 -0500

    fix disc

--- zero_rate_curve/src/bond_price.rs
+++ zero_rate_curve/src/bond_price.rs
@@ -1,16 +1,20 @@
 use std::f64::consts::E;
 
+// Cash flow by date
 struct CFD {
     t: f64,
     cash_flow: f64,
 }
 
-fn disc(t: f64, cash_flow: f64, zero_rate: fn(f64) -> f64) -> f64 {
-    return E.powf(-1.0 * t * zero_rate(cash_flow));
+fn disc(t: f64, zero_rate: fn(f64) -> f64) -> f64 {
+    return E.powf(-1.0 * t * zero_rate(t));
 }
 
-fn get_disc_by_date(dtcf: CFD, zero_rate: fn(f64) -> f64) -> f64 {
-    return disc(dtcf.t, dtcf.cash_flow, zero_rate);
+// dtcfs: Dates to cash flows
+fn bond_price_over_time(dtcfs: Vec<CFD>, zero_rate: fn(f64) -> f64) -> f64 {
+    return dtcfs.into_iter().fold(0.0, |b, dtcf| {
+        return b + dtcf.cash_flow * disc(dtcf.t, zero_rate);
+    });
 }
 
 fn round(x: f64, power: f64) -> f64 {
@@ -24,27 +28,65 @@ mod bond_price_tests {
     #[test]
     fn disc_test() {
         assert_eq!(
-            round(
-                disc(0.5, 0.5, |x: f64| 0.05 + 0.005 * (1.0 + x).sqrt()),
-                6.0
-            ),
+            round(disc(0.5, |x: f64| 0.05 + 0.005 * (1.0 + x).sqrt()), 6.0),
             0.972328
         );
     }
     #[test]
-    fn map_test() {
+    fn iteration_test() {
         assert_eq!(
             round(
-                get_disc_by_date(
-                    CFD {
-                        t: 0.5,
-                        cash_flow: 0.5
-                    },
+                bond_price_over_time(
+                    vec![
+                        CFD {
+                            t: 0.5,
+                            cash_flow: 3.5
+                        },
+                        CFD {
+                            t: 1.0,
+                            cash_flow: 3.5
+                        },
+                        CFD {
+                            t: 1.5,
+                            cash_flow: 3.5
+                        },
+                        CFD {
+                            t: 2.0,
+                            cash_flow: 103.5
+                        },
+                    ],
                     |x: f64| 0.05 + 0.005 * (1.0 + x).sqrt()
                 ),
                 6.0
             ),
-            0.972328
+            101.960379
+        );
+        assert_eq!(
+            round(
+                bond_price_over_time(
+                    vec![
+                        CFD {
+                            t: (1.0 / 12.0),
+                            cash_flow: 3.5
+                        },
+                        CFD {
+                            t: (7.0 / 12.0),
+                            cash_flow: 3.5
+                        },
+                        CFD {
+                            t: (13.0 / 12.0),
+                            cash_flow: 3.5
+                        },
+                        CFD {
+                            t: (19.0 / 12.0),
+                            cash_flow: 103.5
+                        },
+                    ],
+                    |x: f64| 0.05 + 0.005 * (1.0 + x).sqrt()
+                ),
+                6.0
+            ),
+            104.573694
         );
     }
 }
--- zero_rate_curve/src/main.rs
+++ zero_rate_curve/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     use std::time::Instant;
     let now = Instant::now();
 
-    println!("Bond price: ");
+    println!("Run the tests");
 
     let elapsed = now.elapsed();
     println!("\nElapsed: {:.2?}", elapsed);

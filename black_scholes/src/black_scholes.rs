3efcf6ba4b5a172cdd1062c62efc5e1bd3fac719
Kathryn Lovell <krlovell017@gmail.com>
2025-03-22 14:22:30 -0400

    inputs

--- black_scholes/src/black_scholes.rs
+++ black_scholes/src/black_scholes.rs
@@ -1,11 +1,15 @@
 use statrs::distribution::{ContinuousCDF, Normal};
 
-pub fn cdf(x: f64) -> f64 {
+pub fn black_scholes(s: f64, k: f64, sigma: f64, t: f64) -> f64 {
+    return s;
+}
+
+fn cdf(x: f64) -> f64 {
     let n = Normal::new(0.0, 1.0).unwrap();
     return n.cdf(x);
 }
 
-pub fn round(x: f64, power: f64) -> f64 {
+fn round(x: f64, power: f64) -> f64 {
     return (x * 10.0_f64.powf(power)).round() / (10.0_f64.powf(power));
 }
 

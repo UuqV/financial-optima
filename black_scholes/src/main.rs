bba039c9aa6c28f79778bef40dd25df8c9fd1c85
Kathryn Lovell <krlovell017@gmail.com>
2025-03-22 15:06:40 -0400

    formatting

--- black_scholes/src/main.rs
+++ black_scholes/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let now = Instant::now();
     let put: f64 = black_scholes::black_scholes(25.0, 30.0, 0.30, 0.25, 0.02);
     println!(
-        "Put option price based on B-S Formula (S = 25, K = 30, vol = 30%, T = 0.25, r = 2%):\n\n{:#.6}",
+        "10. i)\n\tPut option price based on B-S Formula (S = 25, K = 30, vol = 30%, T = 0.25, r = 2%):\n\n\t{:#.6}",
         put
     );
 
     println!(
-        "\n\nPortfolio value (1000P + 400S + 10000):\n\n{:#.6}\n",
+        "\n\n\tPortfolio value (1000P + 400S + 10000):\n\n\t{:#.6}\n",
         1000.0 * put + 400.0 * 25.0 + 10000.0
     );
 

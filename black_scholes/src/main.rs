d695b22b0fb412ac1f659add5b5dfc237d234526
Kathryn Lovell <krlovell017@gmail.com>
2025-03-22 16:26:50 -0400

    3

--- black_scholes/src/main.rs
+++ black_scholes/src/main.rs
@@ -3,15 +3,49 @@ mod black_scholes;
 fn main() {
     use std::time::Instant;
     let now = Instant::now();
+
+    println!(
+        "3. i)\n\tThe delta of the put option position is -1000N(-d1)\n\t(S = 100, K = 100, vol = 30%, T = 0.5, r = 5%):\n\n\t{:#.6}\n",
+        -1000.0 * black_scholes::delta(100.0, 100.0, 0.30, 0.5, 0.05)
+    );
+
+    let put: f64 = black_scholes::black_scholes(102.0, 100.0, 0.30, 125.0 / 252.0, 0.05);
+    println!(
+        "ii) Put option price based on B-S Formula (S = 100, K = 100, vol = 30%, T = 125/252, r = 5%):\n\n\t{:#.6}",
+        put
+    );
+
     let put: f64 = black_scholes::black_scholes(25.0, 30.0, 0.30, 0.25, 0.02);
     println!(
-        "10. i)\n\tPut option price based on B-S Formula (S = 25, K = 30, vol = 30%, T = 0.25, r = 2%):\n\n\t{:#.6}",
+        "5. i)\n\tPut option price based on B-S Formula (S = 25, K = 30, vol = 30%, T = 0.25, r = 2%):\n\n\t{:#.6}",
         put
     );
 
     println!(
         "\n\n\tPortfolio value (1000P + 400S + 10000):\n\n\t{:#.6}\n",
         1000.0 * put + 400.0 * 25.0 + 10000.0
+    );
+
+    let delta: f64 = black_scholes::delta(25.0, 30.0, 0.30, 0.25, 0.02);
+
+    println!(
+        "ii)\n\tThe delta of the put option position is -1000N(-d1):\n\n\t{:#.6}\n",
+        -1000.0 * delta
+    );
+
+    println!(
+        "\tThe delta of the portfolio is the delta of the put option + 400:\n\n\t{:#.6}\n",
+        -1000.0 * delta + 400.0
+    );
+
+    println!(
+        "\tWe will purchase the above amount of shares for:\n\n\t{:#.6}\n",
+        (1000.0 * delta + 400.0) * 20.0
+    );
+
+    println!(
+        "\tUsing the 10,000 in cash we have and borrowing the rest at the risk-free rate:\n\n\t{:#.6}\n",
+        (1000.0 * delta + 400.0) * 20.0
     );
 
     let elapsed = now.elapsed();

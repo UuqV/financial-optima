74a75417b77bd5fe291b363fe0b0c309ab4cb070
Kathryn Lovell <krlovell017@gmail.com>
2025-03-27 10:28:53 -0400

    fix typos

--- black_scholes/src/main.rs
+++ black_scholes/src/main.rs
@@ -11,13 +11,13 @@ fn main() {
 
     let put: f64 = black_scholes::black_scholes(100.0, 100.0, 0.30, 0.5, 0.05);
     println!(
-        "\tPut option price based on B-S Formula (S = 100, K = 100, vol = 30%, T = 0.5, r = 5%):\n\n\t{:#.6}",
+        "\tP1 Put option price based on B-S Formula (S = 100, K = 100, vol = 30%, T = 0.5, r = 5%):\n\n\t{:#.6}",
         put
     );
 
     let put: f64 = black_scholes::black_scholes(102.0, 100.0, 0.30, 125.0 / 252.0, 0.05);
     println!(
-        "\n\tii) Put option price based on B-S Formula (S = 102, K = 100, vol = 30%, T = 125/252, r = 5%):\n\n\t{:#.6}",
+        "\n\tP2 Put option price based on B-S Formula (S = 102, K = 100, vol = 30%, T = 125/252, r = 5%):\n\n\t{:#.6}",
         put
     );
 
@@ -33,6 +33,8 @@ fn main() {
         "\n\n\tPortfolio value (1000P + 400S + 10000):\n\n\t{:#.6}\n",
         1000.0 * put + 400.0 * 25.0 + 10000.0
     );
+
+    println!("iii)");
 
     black_scholes::rebalance(
         vec![25.0, 30.0, 26.0, 22.0, 27.0],

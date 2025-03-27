8f46585aa2844b0a085ef1be3ea8ceba8f09ed2d
Kathryn Lovell <krlovell017@gmail.com>
2025-03-27 11:04:51 -0400

    first pass

--- zero_rate_curve/src/newton.rs
+++ zero_rate_curve/src/newton.rs
@@ -1,7 +1,29 @@
 use crate::bond_price::{round, CFD};
+use std::f64::consts::E;
+use std::num;
 
-fn newton_bond_price(b: f64, cash_flow_dates: Vec<CFD>, tol: f64) -> f64 {
-    return b;
+fn newton_bond_price(b: f64, cash_flow_dates: &Vec<CFD>, tol: f64) -> f64 {
+    let x0: f64 = 0.1;
+    let mut xnew: f64 = x0;
+    let mut xold: f64 = x0 - 1.0;
+    while (xnew - xold).abs() > tol {
+        xold = xnew;
+        xnew = xold + (upperSum(cash_flow_dates, xold) - b) / lowerSum(cash_flow_dates, xold);
+        println!("{}", xnew);
+    }
+    return xnew;
+}
+
+fn upperSum(cash_flow_dates: &Vec<CFD>, xold: f64) -> f64 {
+    return cash_flow_dates.into_iter().fold(0.0, |i, c| {
+        return c.cash_flow * E.powf(-xold * c.t);
+    });
+}
+
+fn lowerSum(cash_flow_dates: &Vec<CFD>, xold: f64) -> f64 {
+    return cash_flow_dates.into_iter().fold(0.0, |i, c| {
+        return c.cash_flow * c.t * E.powf(-xold * c.t);
+    });
 }
 
 #[cfg(test)]
@@ -10,40 +32,35 @@ mod newton_bond_tests {
 
     #[test]
     fn book_ex() {
+        let cash_flows = vec![
+            CFD {
+                t: (4.0 / 12.0),
+                cash_flow: 4.0,
+            },
+            CFD {
+                t: (10.0 / 12.0),
+                cash_flow: 4.0,
+            },
+            CFD {
+                t: (16.0 / 12.0),
+                cash_flow: 4.0,
+            },
+            CFD {
+                t: (22.0 / 12.0),
+                cash_flow: 4.0,
+            },
+            CFD {
+                t: (28.0 / 12.0),
+                cash_flow: 4.0,
+            },
+            CFD {
+                t: (34.0 / 12.0),
+                cash_flow: 104.0,
+            },
+        ];
+
         assert_eq!(
-            round(
-                newton_bond_price(
-                    105.0,
-                    vec![
-                        CFD {
-                            t: (4.0 / 12.0),
-                            cash_flow: 4.0,
-                        },
-                        CFD {
-                            t: (10.0 / 12.0),
-                            cash_flow: 4.0,
-                        },
-                        CFD {
-                            t: (16.0 / 12.0),
-                            cash_flow: 4.0,
-                        },
-                        CFD {
-                            t: (22.0 / 12.0),
-                            cash_flow: 4.0,
-                        },
-                        CFD {
-                            t: (28.0 / 12.0),
-                            cash_flow: 4.0,
-                        },
-                        CFD {
-                            t: (34.0 / 12.0),
-                            cash_flow: 104.0,
-                        },
-                    ],
-                    0.000001
-                ),
-                4.0
-            ),
+            round(newton_bond_price(105.0, &cash_flows, 0.000001), 4.0),
             6.4502
         );
     }

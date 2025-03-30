a30c804624054c79a898707c229a264b3d52b09b
cdyu <connorkickyu@gmail.com>
2025-03-29 20:25:36 -0400

    Updated Derivations

--- nasty-connor-python/hw5/q4.py
+++ nasty-connor-python/hw5/q4.py
@@ -10,31 +10,32 @@ def delta(S, K, r, sig, T, q):
     return eq * scipy.stats.norm.cdf(d_1)
 
 
-def gamma(S, K, r, sig, T, q):
+def delta_derive_by_k(S, K, r, sig, T, q):
+    # I still think it's bullshit k represents strike 
     d_1 = d1(S, K, r, sig, T, q)
-    first_num = math.exp(-q * T)
-    first_denom = S * sig * math.sqrt(T)
+    num = -1 * math.exp(-1 * q * T) * scipy.stats.norm.pdf(d_1)
+    denom = K * sig * math.sqrt(T)
 
-    return (first_num / first_denom) * scipy.stats.norm.pdf(d_1)
+    return num / denom 
 
 
-def main(xnew, K, r, sig, T, q):
+def main(S, xnew, r, sig, T, q):
     xold = -1000
     tol_approx = 1e-6
     while abs(xold - xnew) > tol_approx:
         xold = xnew
-        sub = (delta(xnew, K, r, sig, T, q) - .5)/gamma(xnew, K, r, sig, T, q)
+        sub = (delta(S, xnew, r, sig, T, q) - .5)/delta_derive_by_k(S, xnew, r, sig, T, q)
         xnew = xnew - sub
         print(xnew)
 
 
-K = 30
+init_K = 30
 sig = .3
 q = .01
 r = .025
-init_S = 30
+S = 30
 t = (3/12)
 
-main(init_S, K, r, sig, t, q)
+main(S, init_K, r, sig, t, q)
 
 

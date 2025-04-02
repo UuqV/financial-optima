97be3d15589744513637ffc00e151aeac4ae7211
Hubert Kasinski <hubertkasinski17@gmail.com>
2025-04-02 14:15:11 +0100

    updated code for homework question :)

--- bootstrap-c++/zero_rate_curve.cpp
+++ bootstrap-c++/zero_rate_curve.cpp
@@ -15,6 +15,7 @@ void printVector(const std::vector<double>& vec)
     std::cout << std::endl;
 }
 
+//Function to generate time intervals for bonds based on duration and frequency of payments per year
 std::vector<double> intervals(double time, double frequency)
 {
     std::vector<double> intervals;
@@ -28,13 +29,13 @@ std::vector<double> intervals(double time, double frequency)
 
     return intervals;
 }
-
+//Function to calculate zero rate for a bond with only one payment
 double IR_a(double B, double C, double t)
 {
     return (1.0 / t) * log(100 / B);
 }
-
-std::vector<double> IR_b(double r_0, double B, double C, double t, double freq) //B price of 6m 
+//Function to calculate zero rate for a bond with 2 payments
+std::vector<double> IR_b(double r_0, double B, double C, double t, double freq) 
 {
     double num = (B - ((C * 100 / 2) * exp(-t*0.5* r_0)));
     double dom = (100 + (C * freq* 100));
@@ -47,7 +48,7 @@ std::vector<double> IR_b(double r_0, double B, double C, double t, double freq) 
 
     return rates;
 }
-
+//Function to calculate zero rate for a bond with more than 2 payments
 std::vector<double> IR_c(std::vector<double> const_rates, double B, double C, double t, double freq, double x0, double tolerance, int max_iter)
 {
     int const_size = const_rates.size(); // size of the existing rates
@@ -124,7 +125,6 @@ std::vector<double> IR_c(std::vector<double> const_rates, double B, double C, do
         for (int i = const_size; i < max; i++)
         {
             double add= -x_coeff[i] * periods[i] * (C * freq * 100) * exp(-periods[i] * ((x_coeff[i] * prev) + (k_coeff[i] * k)));
-            //std::cout << "fdx_add: " << add << "\n";
             fdx += add;
         }
 
@@ -141,73 +141,71 @@ std::vector<double> IR_c(std::vector<double> const_rates, double B, double C, do
 
         prev = curr;
 
-        
+        std::cout << "Iteration : " << iteration << "  Rate: " << curr << "\n";
 
     }
 
+    //calculate the rates for the remaining periods using the coefficients betwween a and b
     for (int i = const_size; i <= max; ++i)
     {
         rates[i] = ((x_coeff[i] * prev) + (k_coeff[i] * k));
     }
 
 
-    return rates;
+    return rates; //returns a vector of the rates from (00) to (0,t)
 }
 
 
 int main()
 {
-    double tol = 0.0000000001;
+    
+    double tol = 0.0000000000001;
     int iter_1 = 20;
-    int iter_2 = 5;
+    
+    double overnight = 0.05; //overnight rate
 
-    std::cout << "Example from class: \n";
+    //6-month
+    //input price = 97.5, coupon rate = 0, time = 0.5years (6months)
+    double six_month = IR_a(97.5, 0, 0.5); 
+    std::cout << "6-month zero-rate: " << six_month << "\n";
 
-    double six_month = IR_a(99, 0, 0.5);
-    std::cout << "\n6-month zero-rate: " << six_month << "\n";
-
-    std::vector<double> one_year_rates = IR_b(six_month, 102, 0.04, 1, 0.5);
+    //1-year
+    //input price = 100, coupon rate = 0.05 (5%), time = 1year, frequency = 0.5 (6months)
+    std::vector<double> one_year_rates = IR_b(six_month, 100, 0.05, 1, 0.5); 
     double one_year = one_year_rates.back();
     std::cout << "\n1-year rate: " << one_year << "\n";
+    
+    //3-year
+    std::cout << "\n3-year rate convergence: \n";
+    //input price = 102, coupon rate = 0.05 (5%), time = 3years, frequency = 0.5 (6months), inital guess= 0.05, tolerance, max_iterations
+    std::vector<double> three_year_rates = IR_c(one_year_rates, 102, 0.05, 3.0, 0.5, 0.05, tol, iter_1);  
+    double three_year = three_year_rates.back();
+    std::cout << "\n3-year rate: " << three_year << "\n";
 
-    std::vector<double> two_year_rates = IR_c(one_year_rates, 103.5, 0.04, 2.0, 0.5, 0.05, tol, iter_1);
-    double two_year = two_year_rates.back();
-    std::cout << "\n2-year rate: " << two_year << "\n";
+	std::vector<double> three_year_periods = intervals(3.0, 0.5);
 
-    std::vector<double> five_year_rates = IR_c(two_year_rates, 109, 0.04, 5.0, 0.5, 0.05, tol, iter_1);
+	std::cout << "r(0,0) = " << overnight << " (overnight rate)\n";
+
+	for (int i = 0; i < three_year_periods.size(); i++)
+	{
+		std::cout << "r(0," << three_year_periods[i] << ") = " << three_year_rates[i] << "\n";
+	}
+    
+    //5-year
+    std::cout << "\n5-year rate convergence: \n";
+    //input price = 104, coupon rate = 0.05 (5%), time = 5years, frequency = 0.5 (6months), inital guess= 0.05, tolerance, max_iterations
+    std::vector<double> five_year_rates = IR_c(three_year_rates, 104, 0.06, 5.0, 0.5, 0.05, tol, iter_1);
     double five_year = five_year_rates.back();
     std::cout << "\n5-year rate: " << five_year << "\n";
 
-    std::cout << "Q7 from exercises: \n";
+    std::vector<double> five_year_periods = intervals(5.0, 0.5);
 
-    double three_month_tbill = IR_a(98.7, 0, 0.25);
-    std::cout << "\n3-month zero-rate: " << three_month_tbill << "\n";
-   
-    double six_month_tbill = IR_a(97.5, 0, 0.5);
-    std::cout << "\n6-month zero-rate: " << six_month_tbill << "\n";
-   
-    std::vector<double> rates_in;
-    rates_in.push_back(six_month_tbill); //input has to be vector but it can be a vector of size 1
-   
-    double price_2y = 100.0 + (5.0 / 32.0);
-    std::vector<double> twoyear_rates = IR_c(rates_in, price_2y, 0.04875, 2.0, 0.5, 0.05, tol, iter_1);
-    double twoyear = twoyear_rates.back();
-    std::cout << "\n2-year bond rate: " << twoyear << "\n";
-   
-    double price_3y = 100.0 + (5.0 / 32.0);
-    std::vector<double> threeyear_rates = IR_c(twoyear_rates, price_3y, 0.04875, 3.0, 0.5, 0.05, tol, iter_1);
-    double threeyear = threeyear_rates.back();
-    std::cout << "\n3-year bond rate: " << threeyear << "\n";
-   
-    double price_5y = 99.0 + (22.0 / 32.0);
-    std::vector<double> fiveyear_rates = IR_c(threeyear_rates, price_5y, 0.04625, 5.0, 0.5, 0.05, tol, iter_1);
-    double fiveyear = fiveyear_rates.back();
-    std::cout << "\n5-year bond rate: " << fiveyear << "\n";
-   
-    double price_10y = 101.0 + (4.0 / 32.0);
-    std::vector<double> tenyear_rates = IR_c(fiveyear_rates, price_10y, 0.04875, 10.0, 0.5, 0.05, tol, iter_1);
-    double tenyear =tenyear_rates.back();
-    std::cout << "\n10-year bond rate: " << tenyear << "\n";
+    std::cout << "r(0,0) = " << overnight << " (overnight rate)\n";
+
+    for (int i = 0; i < five_year_periods.size(); i++)
+    {
+        std::cout << "r(0," << five_year_periods[i] << ") = " << five_year_rates[i] << "\n";
+    }
 
     return 0;
 }

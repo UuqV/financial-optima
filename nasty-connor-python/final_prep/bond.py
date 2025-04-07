783d0c0690c7bf4c74fc017490e888f5b33af263
cdyu <connorkickyu@gmail.com>
2025-04-07 12:46:56 -0400

    Updated day before final

--- nasty-connor-python/final_prep/bond.py
+++ nasty-connor-python/final_prep/bond.py
@@ -1,151 +1,120 @@
 import math
 
-# TODO: Input variables
-face = 100
-cpn_freq = 6/12
-cpn_rate = .04
-maturity = 28/12
 
 def risk_free_rate(x):
-    # TODO: Input Formula calculation 
+    # TODO: Input Formula calculation
     return .015 + (1 + 2 * math.pow(x, 2))/(100 + 100 * math.pow(x, 2))
 
 
-cash_flows = [] # time, cash flow, discount rate, pv
+def calculate_cash_flows(calc_time, cpn_payment):
+    cash_flows = [] # time, cash flow, discount rate, pv
+    while calc_time > 0:
+        if calc_time == maturity:
+            cash_flow = face + cpn_payment
+        else:
+            cash_flow = cpn_payment
 
-calc_time = maturity
-cpn_payment = cpn_rate * face * cpn_freq
-while calc_time > 0:
-    if calc_time == maturity:
-        cash_flow = face + cpn_payment
-    else:
-        cash_flow = cpn_payment
+        discount = risk_free_rate(calc_time)
+        pv = cash_flow * math.exp(-calc_time * discount)
 
-    discount = risk_free_rate(calc_time)
-    pv = cash_flow * math.exp(-calc_time * discount)
-
-    cash_flows.append((calc_time, cash_flow, discount, pv))
-    calc_time -= cpn_freq
-
-
-
-cash_flows.reverse()
-print(cash_flows)
-price = sum(cash_flow[3] for cash_flow in cash_flows)
-all_cpns = [cash_flow[0] for cash_flow in cash_flows]
-
-#___________________________ Copy of Bootstrapping inputs
-
-# TODO: Input variables 
-bonds = [
-    (28/12, cpn_rate * 100, price, True)
-]
-initial_r = .1
-result = [(0, risk_free_rate(0))] # Time yield 
-newton_output = []
-OFFSET = -2/12
-
-#__________________________ Copy of Bootstrapping
+        cash_flows.append((calc_time, cash_flow, discount, pv))
+        calc_time -= cpn_freq
+    cash_flows.reverse()
+    return cash_flows
 
 
-def print_ascii_table(float_list):
-    print(f"{'Index':<8}{'Guesses'}")
-    print('-' * 20)  # Line separator
+def sum_up_cashflows(yield_guess, price, cash_flows):
+    pv_sum = 0
+    for time, cash_flow, _, _ in cash_flows:
+        pv_sum += cash_flow * math.exp(-yield_guess * time)
+    return pv_sum - price
 
-    # Print each index and corresponding rounded float value
-    for i, num in enumerate(float_list, start=1):
-        print(f"{i:<8}{num:.6f}")
+def sum_up_deriv(yield_guess, cash_flows):
+    # Duplicate of bond_first_deriv
+    pv_sum = 0
+    for time, cash_flow, _, _ in cash_flows:
+        pv_sum += time * cash_flow * math.exp(-yield_guess * time)
 
-def price_guess(guess_r, cpn_total_payout, cpn_payment, bond, result_correct=False):
-    time, _, recorded_price, write_res = bond
-    last_bond_time, last_bond_yield = result[-1]
-    cpn_time = last_bond_time + cpn_freq + OFFSET
+    return pv_sum
 
-    while cpn_time < time:
-        # Calculate the time between coupon and last bond to offset it and
-        # guessing bond to last bond for slope
-        cpn_time_diff = cpn_time - last_bond_time
-        guess_time_diff = time - last_bond_time
-        slope = (guess_r - last_bond_yield) / guess_time_diff  # linear slopt of the yield
-        slope_time_adjusted = last_bond_yield + (slope * cpn_time_diff)
-        if write_res:
-            print(f"Slope time adjusted/yield for {cpn_time} is {slope_time_adjusted}")
-        if result_correct:
-            result.append((cpn_time, slope_time_adjusted))
-
-        cpn_total_payout += cpn_payment * math.exp(-1 * cpn_time * slope_time_adjusted)
-        cpn_time += cpn_freq
-
-    total_payout = cpn_total_payout + (face + cpn_payment) * math.exp(-1 * time * guess_r)
-
-    return total_payout - recorded_price
+def bond_yield_calculation(initial_r, price, cash_flows, tol):
+    # cash_flows = time, cash flow, discount rate, pv
+    xold = -1000
+    xnew = initial_r
+    while abs(xold-xnew) > tol:
+        xold = xnew
+        num = sum_up_cashflows(xnew, price, cash_flows)
+        denom = sum_up_deriv(xnew, cash_flows)
+        xnew = xnew + num/denom
+        print(xnew)
+    return xnew
 
 
-def price_deriv(guess_r, cpn_payment, bond):
-    time, _, _, _ = bond
-    last_bond_time, last_bond_yield = result[-1]
-    cpn_time = last_bond_time + cpn_freq + OFFSET
+# bootstrap_zerorate(initial_r, face, cpn_freq, result, bonds, OFFSET, newton_output)
+# print(f"Now result is {result}")
 
-    deriv_slope = 1 / (time - last_bond_time)
+def bond_first_deriv(cash_flows, calc_yield):
+    # tici e ^ (-yti)
+    pv_sum = 0
+    for time, cash_flow, _, _ in cash_flows:
+        pv_sum += time * cash_flow * math.exp(-calc_yield * time)
+    return pv_sum
 
-    cpn_deriv = 0
+def bond_second_deriv(cash_flows, calc_yield):
+    pv_sum = 0
+    for time, cash_flow, _, _ in cash_flows:
+        pv_sum += math.pow(time, 2) * cash_flow * math.exp(-calc_yield * time)
+    return pv_sum
 
-    while cpn_time < time:
-
-        # Calculate the time between coupon and last bond to offset it
-        # and guessing bond to last bond for slope
-        cpn_time_diff = cpn_time - last_bond_time
-        guess_time_diff = time - last_bond_time
-        slope = (guess_r - last_bond_yield) / guess_time_diff
-        slope_time_adjusted = last_bond_yield + (slope * cpn_time_diff)
-
-        base_payment = cpn_payment * math.exp(-1 * cpn_time * slope_time_adjusted)
-
-        # Calculate derivative of exponential discount
-        deriv_of_exp = -cpn_time * deriv_slope * cpn_time_diff
-
-        cpn_deriv += deriv_of_exp * base_payment
-        cpn_time += cpn_freq
-
-    last_payment = -time * (face + cpn_payment) * math.exp(-1 * time * guess_r)
-    # print(f"Calculated price for {guess_r} is {cpn_deriv + last_payment}\n")
-    return cpn_deriv + last_payment
+def print_ascii_table(time_rate_list):
+    # Header
+    header = f"{'Time':>10} | {'Rate':>20}"
+    separator = "-" * len(header)
+    print(header)
+    print(separator)
+    
+    # Rows
+    for time, rate in time_rate_list:
+        print(f"{time:10.8f} | {rate:20.8f}")
 
 
-prev_time = None
-for bond in bonds:
-    time, cpn_rate, price, write_res = bond
-    cpn_total_payout = 0
-    cpn_payment = face * (cpn_rate * cpn_freq / 100)
+def num_calcs(cash_flows, price, calc_yield):
+    deriv = sum_up_deriv(calc_yield, cash_flows)
+    duration = (1/price) * deriv
 
-    for i in result:
-        prev_time, prev_yield = i
-        if prev_time == 0:
-            continue
-        cpn_total_payout += cpn_payment * math.exp(-1 * prev_time * prev_yield)
+    second_deriv = bond_second_deriv(cash_flows, calc_yield)
+    convexity = (1/price) * second_deriv
 
-    if time == .5 or (len(result) > 0 and result[-1][0] == time - cpn_freq):
-        r = math.log((price - cpn_total_payout) / (face + cpn_payment)) / (-1 * time)
-        result.append((time, r))
-        continue
+    dollar_duration = -deriv
 
-    guess_old = -1000
-    guess_r = initial_r
-    tol_approx = 1e-6
-    print(f"Beginning estimation for {time} with {guess_r}")
-    while abs(guess_old - guess_r) > tol_approx:
-        guess_old = guess_r
-        sub = price_guess(guess_r, cpn_total_payout, cpn_payment, bond) / price_deriv(guess_r, cpn_payment, bond)
-        guess_r = guess_r - sub
-        if bond[3]:
-            newton_output.append(guess_r)
-        print(f"New guess for {time} is {guess_r}")
+    dollar_convexity = second_deriv
 
-    price_guess(guess_r, cpn_total_payout, cpn_payment, bond, True)
-    result.append((time, guess_r))
-    print(f"Bond at time {time} has rate: {guess_r}")
-    if bond[3]:
-        print_ascii_table(newton_output)
+    dv01 = dollar_duration / 10_000
 
-print(result)
-print(f"PRICE WAS {price}")
+    print(f"Price is {price}")
+
+    print(f"Duration is {duration}")
+    print(f"Convexity is {convexity}")
+
+    print(f"Dollar Duration is {dollar_duration}")
+    print(f"Dollar Convexity is {dollar_convexity}")
+
+    print(f"DV01 is {dv01}")
+
+
+if __name__ == "__main__":
+    face = 100
+    cpn_freq = 6 / 12
+    cpn_rate = .04
+    maturity = 28 / 12
+    calc_time = maturity
+    cpn_payment = cpn_rate * face * cpn_freq
+    cash_flows = calculate_cash_flows(calc_time, cpn_payment)
+    initial_r = .1
+
+    price = sum(cash_flow[3] for cash_flow in cash_flows)
+    calc_yield = bond_yield_calculation(initial_r, price, cash_flows, tol=1e-6)
+    num_calcs(cash_flows, price, calc_yield)
+
+# Compute the modified duration, dollar duration, DV01, convexity, and
+# dollar convexity of the bond.
